45128819493eb6b6f0a1d1b6dcd385086ba3713d
JRamone <jmkoho@gmail.com>
2020-09-17 16:47:22 +0300

    to heroku

--- osa4/blogit/index.js
+++ osa4/blogit/index.js
@@ -1,6 +1,7 @@
 const config = require('./utils/config')
 const app = require('./app')
+const PORT = process.env.PORT || 3001
 
-app.listen(config.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
